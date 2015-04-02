e618260935667b55020e4ed93426d42892623767
Ernie Salazar <ehsalazar75@gmail.com>
2015-04-02 14:11:21 -0700

    Update todos controller to createTodo.

--- app/controllers/todos.js
+++ app/controllers/todos.js
@@ -1,4 +1,17 @@
 import Ember from 'ember';
 
-export default Ember.Controller.extend({
+export default Ember.ArrayController.extend({
+  actions: {
+    createTodo: function(newTitle) {
+      // create the new todo
+      var todo = this.store.createRecord('todo', {
+        title: newTitle,
+        isCompleted: false
+      });
+      // clear the 'new todo' text field
+      this.set('newTitle', '');
+      // save the new model
+      todo.save();
+    }
+  }
 });
